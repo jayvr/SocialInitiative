0df72240fd9577186ad49e97598d1e8a2d9ea1f5
Jay Vanraj <jayvanraj2000@gmail.com>
2020-04-30 19:08:19 +0530

    minor changes

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,22 +1,8 @@
 auth.onAuthStateChanged(user => {
     if (user) {
-    console.log("User logged in : ",user);
+        console.log("User logged in : ", user);
     }
     else {
-        console.log("user logged out!!!")
+        console.log("user logged out!!!");
     }
 });
-
-const nav = document.querySelector("#nav")
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("nav").style.top = "0";
-    } else {
-        document.getElementById("nav").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
--- JS/newActivity.js
+++ JS/newActivity.js
@@ -1,6 +1,6 @@
 auth.onAuthStateChanged(user => {
     if (user) {
-    console.log("User logged in : ",user);
+        console.log("User logged in : ", user);
     }
     else {
         console.log("user logged out!!!")
