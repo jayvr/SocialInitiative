9e1cf68e18be2f41cddd6306cd58907c3c398083
Jay Vanraj <jayvanraj2000@gmail.com>
2020-04-30 13:07:53 +0530

    gettind data1

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,28 +1,34 @@
 const itm_lst = document.querySelector("#item-list")
+// const itm_lst = document.querySelector("#item-list")
 
 let user = db.collection("users")
 let act = db.collection("activity")
 
 function addList(doc) {
     let li = document.createElement("li")
-    let name = document.createElement("span")
-    let email = document.createElement("span")
-    let phone = document.createElement("span")
+    let title = document.createElement("span")
+    let desc = document.createElement("span")
+    let time = document.createElement("span")
+    let author = document.createElement("span")
 
     li.setAttribute("doc-id", doc.id)
-    name.textContent = doc.data().name
-    email.textContent = doc.data().email
-    phone.textContent = doc.data().phone
+    title.textContent = doc.data().title
+    desc.textContent = doc.data().description
+    time.textContent = doc.data().time
+    // author.textContent = doc.data().author
 
-    li.appendChild(name);
-    li.appendChild(email);
-    li.appendChild(phone);
+    // author.textContent = user.doc(doc.data().author).name
+
+    li.appendChild(title);
+    li.appendChild(desc);
+    // li.appendChild(author);
+    li.appendChild(time);
 
     itm_lst.appendChild(li);
 
 }
 
-user.get().then(snap => {
+act.get().then(snap => {
     snap.docs.forEach(doc => {
         addList(doc)
     })
