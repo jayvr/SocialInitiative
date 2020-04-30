feca5f2eddfe983d9a5a3fb757baf26c90a6b901
Devarsh <devarshdave6703@gmail.com>
2020-04-30 20:15:09 +0530

    console log

--- JS/User_state.js
+++ JS/User_state.js
@@ -1,9 +1,11 @@
 auth.onAuthStateChanged(user => {
     if (user) {
     const STATUS = true;
+    console.log("User Logged in!!!!");
     const Loged_user = user;
     }
     else {
         const STATUS = false;
+        console.log("User Logged out!!!!");
     }
 });
