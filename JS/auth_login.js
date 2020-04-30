724070bb057a32b8e42c1b7f7ebaf7991ed88474
Devarsh <devarshdave6703@gmail.com>
2020-04-30 12:22:41 +0530

    added auth_login js file !!!login still is incomplete

---
+++ JS/auth_login.js
@@ -0,0 +1,8 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const login_btn = document.querySelector("#login_btn");
+login_btn.addEventListener("click", (e) => {
+    const em = email.value;
+    const pass = password.value;
+    console.log(em,pass);
+})
