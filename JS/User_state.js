8c186793332aee0a031790da035886fde6d099b4
Devarsh <devarshdave6703@gmail.com>
2020-04-30 20:12:49 +0530

    User object in user_state file

--- JS/User_state.js
+++ JS/User_state.js
@@ -1,8 +1,9 @@
 auth.onAuthStateChanged(user => {
     if (user) {
     const STATUS = true;
+    const Loged_user = user;
     }
     else {
-        console.log("user logged out!!!")
+        const STATUS = false;
     }
 });
